daa1ed9db41d6bc90109f14d00fce74077d103ac
pepe <pepe@DESKTOP-JRPKCDE>
2025-11-24 05:21:40 +0300

    Task3

---
+++ Tyuiu.KhafizovTV.Sprint5.Task3.V12.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KhafizovTV.Sprint5.Task3.V12.Lib
+{
+    public class DataService : ISprint5Task3V12
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string tempPath = Path.GetTempPath();
+            string fileName = "OutPutFileTask3.bin";
+            string filePath = Path.Combine(tempPath, fileName);
+
+            double y = Math.Round(Math.Pow(x, 3) / Math.Pow(2 * (x + 5),2), 3);
+
+            byte[] binData = BitConverter.GetBytes(y);
+
+            File.WriteAllBytes(filePath, binData);
+
+            return filePath;
+        }
+    }
+}
---
+++ Tyuiu.KhafizovTV.Sprint5.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KhafizovTV.Sprint5.Task3.V12.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint5.Task3.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            DataService ds = new DataService();
+
+            string path = ds.SaveToFileTextData(3);
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
