528cafafe924c156063c2c5833306c0e14a19124
pepe <pepe@DESKTOP-JRPKCDE>
2025-11-24 05:11:06 +0300

    Task2

---
+++ Tyuiu.KhafizovTV.Sprint5.Task2.V29.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KhafizovTV.Sprint5.Task2.V29.Lib
+{
+    public class DataService : ISprint5Task2V29
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string tempPath = Path.GetTempPath();
+            string fileName = "OutPutFileTask2.csv";
+            string filePath = Path.Combine(tempPath, fileName);
+            string csvContent = "";
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int value = matrix[i, j];
+
+                    if (value % 2 != 0)
+                    {
+                        value = 0;
+                    }
+
+                    csvContent += value.ToString();
+
+                    if (j < 2) csvContent += ";";
+                }
+                if (i < 2) csvContent += "\n";
+            }
+
+            File.WriteAllText(filePath, csvContent);
+
+            return filePath;
+        }
+    }
+}
---
+++ Tyuiu.KhafizovTV.Sprint5.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KhafizovTV.Sprint5.Task2.V29.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint5.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            DataService ds = new DataService();
+
+            int[,] array = {
+                {9, 2, 5},
+                {3, 2, 4},
+                {2, 8, 8}
+            };
+
+            string filePath = ds.SaveToFileTextData(array);
+
+            Assert.IsTrue(File.Exists(filePath));
+
+            string content = File.ReadAllText(filePath);
+            string expected = "0;2;0\n0;2;4\n2;8;8";
+
+            Assert.AreEqual(expected, content);
+        }
+    }
+}
