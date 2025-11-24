0121f30336ee656948fa4be089a97807d15486fd
pepe <pepe@DESKTOP-JRPKCDE>
2025-11-24 08:27:26 +0300

    jt6j65

--- Tyuiu.KhafizovTV.Sprint5.Task7.V16.Lib/DataService.cs
+++ Tyuiu.KhafizovTV.Sprint5.Task7.V16.Lib/DataService.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.KhafizovTV.Sprint5.Task7.V16.Lib
@@ -8,10 +7,15 @@ namespace Tyuiu.KhafizovTV.Sprint5.Task7.V16.Lib
     {
         public string LoadDataAndSave(string path)
         {
+
             string content = File.ReadAllText(path);
-            string pattern = @"\b\S{2}\b";
+
+            string pattern = @"\b\w{2}\b";
+
             string result = Regex.Replace(content, pattern, "XY");
-            string outputPath = @"C:\DataSprint5\OutPutDataFileTask7V16.txt";
+
+            string outputPath = Path.GetTempFileName();
+
             File.WriteAllText(outputPath, result);
 
             return outputPath;
